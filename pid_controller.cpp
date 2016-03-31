1234111f002e769a7e94963df2d683e21f620465
Kevin Huo <kevinhuo22@gmail.com>
2016-03-31 11:36:57 -0400

    Added documentation and revised the class variables.

--- pid_controller.cpp
+++ pid_controller.cpp
@@ -1,9 +1,9 @@
 #include "pid_controller.h"
 
-PID::PID(kp_val, ki_val, kd_val, min, max)
+PID::PID(double kp_val, double ki_val, double kd_val, double min, double max)
 {
   set_tunings(kp_val, ki_val, kd_val);
-  set_output_limits(double min, double max);
+  set_output_limits(min, max);
 }
 
 double PID::compute(double set_point, double current_point)
@@ -11,15 +11,20 @@ double PID::compute(double set_point, double current_point)
   double current_time = boost::posix_time::ptime microsec_clock::universal_time();
   double error = current_point - set_point;
   double delta_time = current_time - previous_time;
-  //Reiman sum
-  double error_integral = error * (current_time - previous_time);
-  //for derivative
+
+  //Calculate the area under the error curve
+  double error_integral = error * delta_time;
+
+  //Calculate the derivative (change in error / change in time)
   double delta_error = (error - previous_error) / delta_time;
-  //calculate PID output
+
+  //Calculate PID output
   double output = kp * error + ki * error_integral + kd * delta_error;
 
+  //Make sure output is within the min/max limits
   output = check_limits(output);
 
+  //Save current values to be used in the calculations for next iteration
   previous_output = output;
   previous_error = error;
   previous_time = current_time;
@@ -44,15 +49,15 @@ double PID::check_limits(double output)
 {
   if(output >= max)
   {
-  	return max;
+    return max;
   }
   else if(output <= min)
   {
-  	return min;
+    return min;
   }
   else
   {
-  	return output;
+    return output;
   }
 }
 
--- pid_controller.h
+++ pid_controller.h
@@ -6,13 +6,12 @@
 #ifndef PID_CONTROLLER_H_
 #define PID_CONTROLLER_H_
 
-#include <cmath>
 #include <cstdlib>
 #include "boost/date_time/posix_time/posix_time.hpp"
 
 class PID{
   public:
-    PID();
+    PID(double kp_val, double ki_val, double kd_val, double min, double max);
     double compute(double set_point, double current_point);
     void set_kp(double kp_val);
     void set_ki(double ki_val);
@@ -23,10 +22,9 @@ class PID{
 
   private:
     double check_limits(double output);
-    double kp = 0.0, ki = 0.0, kd = 0.0, 
-    previous_output = 0.0, previous_error = 0.0, previous_time = 0.0
-    set_point = 0.0, current_point = 0.0
-    min_output = 0.0, max_output = 0.0;
+    double kp = 0.0, ki = 0.0, kd = 0.0, previous_output = 0.0, 
+        previous_error = 0.0, previous_time = 0.0, min_output = 0.0,
+        max_output = 0.0;
 };
 
 #endif
